c6b46bd9516c5964058f42b64cf55716225a39f6
Andy Dang <andy.dang.it@gmail.com>
2021-07-30 20:13:04 -0500

    Adding useState to refresh value

--- src/Component/pages/Home.js
+++ src/Component/pages/Home.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 const  Home = () => {
+    // let name = "mario";
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
 
-    const handleClick = (e) => {
-        console.log('hello, ninjas', e);
+
+    const handleClick = () => {
+       setName('Snake Eye');
+       setAge( 88);
     }
 
-    const handleClickAgain= (name, e) => {
-        console.log('Hello ' + name, e.target);
-    }
 
     return (  
         <div className="home">
             <h2> Homepage</h2>
+            <p> { name } is { age } year old </p>
             <button onClick={handleClick}> Click Me </button>
 
-            {/* arrow function to prevent from invoke  */}
-            <button onClick={(e) => {
-                handleClickAgain('mario', e)
-            }}> Click Me Again </button>
+
         </div>
     );
 }
