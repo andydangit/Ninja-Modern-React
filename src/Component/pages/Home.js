2cc1cef5c64366aa0c687b6872686c5e65e543f5
pandoragear <andy.dang.it@gmail.com>
2021-07-31 18:45:30 -0500

    useEffect => it re-render change info

--- src/Component/pages/Home.js
+++ src/Component/pages/Home.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import BlogList from '../BlogList';
 
 
@@ -14,6 +14,12 @@ const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
 }
+
+
+useEffect(() => {
+    console.log("use effect ran");
+    console.log(blogs);
+});
 
     return (  
         <div className="home">
