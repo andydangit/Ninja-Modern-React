1a4011a51101586cf226a62f0ef09a3b7c8b33ab
Andy Dang <andy.dang.it@gmail.com>
2021-07-30 19:46:57 -0500

    Add arrow function to prevent from invoke

--- src/Component/pages/Home.js
+++ src/Component/pages/Home.js
@@ -6,10 +6,19 @@ const  Home = () => {
         console.log('hello, ninjas');
     }
 
+    const handleClickAgain= (name) => {
+        console.log('Hello ' + name);
+    }
+
     return (  
         <div className="home">
             <h2> Homepage</h2>
             <button onClick={handleClick}> Click Me </button>
+            
+            {/* arrow function to prevent from invoke  */}
+            <button onClick={() => {
+                handleClickAgain('mario')
+            }}> Click Me Again </button>
         </div>
     );
 }
