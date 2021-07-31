91c8a28c792605b94b9c85408a1f8437b1a54b42
Andy Dang <andy.dang.it@gmail.com>
2021-07-30 20:51:26 -0500

    Add useState for outputting list

--- src/Component/pages/Home.js
+++ src/Component/pages/Home.js
@@ -3,9 +3,20 @@ import React, {useState} from 'react';
 
 const  Home = () => {
 
+const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Snake Eye', id: 2},
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Joe', id: 3}
+]); 
+
     return (  
         <div className="home">
-            <h2> Homepage</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2> {blog.title } </h2>
+                    <p> written by {blog.author} </p>
+                     </div>
+            ))}
         </div>
     );
 }
