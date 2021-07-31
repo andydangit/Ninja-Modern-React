f868c13a6d4e50a861e38dad8be00a3b6aab8267
Andy Dang <andy.dang.it@gmail.com>
2021-07-30 19:49:43 -0500

    Add event to component

--- src/Component/pages/Home.js
+++ src/Component/pages/Home.js
@@ -2,22 +2,22 @@ import React from 'react';
 
 const  Home = () => {
 
-    const handleClick = () => {
-        console.log('hello, ninjas');
+    const handleClick = (e) => {
+        console.log('hello, ninjas', e);
     }
 
-    const handleClickAgain= (name) => {
-        console.log('Hello ' + name);
+    const handleClickAgain= (name, e) => {
+        console.log('Hello ' + name, e.target);
     }
 
     return (  
         <div className="home">
             <h2> Homepage</h2>
             <button onClick={handleClick}> Click Me </button>
-            
+
             {/* arrow function to prevent from invoke  */}
-            <button onClick={() => {
-                handleClickAgain('mario')
+            <button onClick={(e) => {
+                handleClickAgain('mario', e)
             }}> Click Me Again </button>
         </div>
     );
