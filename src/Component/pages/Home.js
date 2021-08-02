cd585997150ddce1296879fa8698ac3f0d60774d
Andy Dang <andy.dang.it@gmail.com>
2021-08-02 14:16:12 -0500

    Add conditional rendering to catch error

--- src/Component/pages/Home.js
+++ src/Component/pages/Home.js
@@ -6,11 +6,12 @@ const  Home = () => {
 
 const [blogs, setBlogs] = useState(null); 
 const [isPending, setIsPending] = useState(true);
+const [error, setError ] =useState(null);
 
 
 
 useEffect(() => {
-    fetch('http://localhost:8000/blogs')
+    fetch('http://localhost:8000/blogss')
     .then(res => {
         // if connect to server but cannot get data will throw error
         if(!res.ok) {
@@ -23,12 +24,13 @@ useEffect(() => {
         setIsPending(false);
     })
     .catch(err => {
-        console.log(err.message);
+        setError(err.message);
     })
     }, []);
 
     return (  
         <div className="home">
+            { error && <div> { error } </div>}
             {isPending && <div> Loading... </div> }
             {blogs && <BlogList blogs={blogs} title="All Blogs!"  /> }
         </div>
