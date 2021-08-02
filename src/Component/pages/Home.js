8f9a2a44e1c3c4f5e78721a8d81a315656de844f
Andy Dang <andy.dang.it@gmail.com>
2021-08-02 14:09:51 -0500

    Add catch error if cannot laod

--- src/Component/pages/Home.js
+++ src/Component/pages/Home.js
@@ -17,7 +17,10 @@ useEffect(() => {
     .then(data => {
         setBlogs(data);
         setIsPending(false);
-    });
+    })
+    .catch(err => {
+        console.log(err.message);
+    })
     }, []);
 
     return (  
