c002e88e7d04ec960ca7ecd61163df4b45d6e156
pandoragear <andy.dang.it@gmail.com>
2021-08-02 19:22:42 -0500

    import react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Navbar from './Component/Navbar';
 import Home from './Component/pages/Home';
+import { BrowserRouter as Router, Route, Switch  } from 'react-router-dom';
 
 function App() {
   return (
+    <Router> 
     <div className="App">
       <Navbar />
       <div className="content">
         <Home />
       </div>
     </div>
+    </Router> 
   );
 }
 
