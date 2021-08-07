9b5a689b05cd94f74acad5f2975ac893bd223bd2
Andy Dang <andy.dang.it@gmail.com>
2021-08-06 20:11:53 -0500

    Add useState to show the title info

--- src/Component/pages/Create.js
+++ src/Component/pages/Create.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+
+
+
 
 const Create = () => {
+const [title, setTitle] = useState('');
+
+
     return ( 
         <div className ="create">
             <h2> Add a New Blog </h2>
@@ -9,6 +16,8 @@ const Create = () => {
                 <input
                 type = "text"
                 required
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
                 />
                 <label>Blog body:</label>
                 <textarea 
@@ -20,6 +29,8 @@ const Create = () => {
                     <option value="yoshi"> Yoshi</option>
                 </select>
                 <button> Add Blog </button>
+                {/* bottom will show the updated title  */}
+                <p> {title } </p>
             </form>
         </div>
      );
